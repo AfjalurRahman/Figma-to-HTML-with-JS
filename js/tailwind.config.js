0674306fce87034d89fec02bded4ca76bf5d4d94
AfjalurRahman <arsag249@gmail.com>
2023-11-13 09:06:19 +0600

    New Project

---
+++ js/tailwind.config.js
@@ -0,0 +1,24 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        // Color Start
+        colors: {
+            headerBg: 'rgba(23, 13, 110, 0.5)',
+            hoverColor: '#1BBF00',
+
+
+        },
+        // Color End
+        // Container Part Start
+        maxWidth: {
+            headerContainer: '1170px',
+          },
+        // Container Part Start
+        // Font Family Start
+        fontFamily: {
+            'openS': ['Open Sans', 'sans-serif'],
+          }, 
+        // Font Family Start
+    }
+    }
+  }
